b0b6dbd3e7bd6bb8a40ca6f5ca7844453ccc881f
calebcox725 <caleb.cox@flatironschool.com>
2016-11-07 09:20:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  
+  function animalWithDevice(deadlyDevice) {
+    return {animalType, deadlyDevice};
+  }
+
+  return animalWithDevice;
+}
+
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+
+var sharkWithFrickinCannon = sharkCreator('Cannon');
